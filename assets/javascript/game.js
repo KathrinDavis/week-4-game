03daf146e5e928236ede9b993ed43d89db6c8b0b
KathrinDavis <kathrinjdavis@gmail.com>
2017-04-18 00:21:19 -0700

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,19 +21,19 @@ $(document).ready(function() {
 	//update numbers to start game
 	function initialize() {
 		//updates var targetNumber with new random
-		targetNumber = Math.floor(Math.random()*120)+1;
+		targetNumber = Math.floor(Math.random() * 120) + 1;
 		//print new targetNumber in targetScore div
 		$("#targetScore").text(targetNumber);
 		score = 0;
-		$("total-score").text(score);
-		$("img").each(function(){
-			$(this).attr("value", Math.floor(Math.random()*12)+1);
+		$("#total-score").text(score);
+		$("img").each(function() {
+			$(this).attr("value", Math.floor(Math.random()*12) + 1);
 		});
 	}
 
 
 //click catcher for crystals to add to score
-$("#crystals").on("click", function() {
+$(".rock").on("click", function() {
 	var crystalValue = ($(this).attr("value"));
     crystalValue = parseInt(crystalValue);
     score += crystalValue;
@@ -41,7 +41,7 @@ $("#crystals").on("click", function() {
 
  //win/lose conditions
  //adds win and resets game
- 	if(score === targetNumber){
+ 	if(score === targetNumber) {
  		wins++;
  		$("#wins-losses").html("<h3>Wins: "+ wins + "</h3><br><h3>Losses: "+ losses + "</h3>");
  		initialize();
